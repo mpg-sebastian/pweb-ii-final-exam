50c4000ce0d42769b1809db663aba6529bc399db
Sebastian Silva <mpg.sebastian@gmail.com>
2022-12-14 23:01:59 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Footer from "./common/footer";
+import Header from "./common/header";
+import Layout from "./common/layout";
+import Carrito from "./pages/Carrito";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Layout>
+        <Header />
+        <main>
+          <Routes>
+            <Route index path="/" element={<Home />} />
+            <Route index path="/carrito" element={<Carrito />} />
+            <Route path="*" element={<h2>La página no exíste.</h2>} />
+          </Routes>
+        </main>
+        <Footer />
+      </Layout>
     </div>
   );
 }
